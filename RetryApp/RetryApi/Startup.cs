4cdc253da998bdebb505f636a58131eb332c68f9
Glenn Condron <glennc@microsoft.com>
2018-09-11 14:41:51 -0700

    initial demo code

---
+++ RetryApp/RetryApi/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace RetryApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<RandomFailureMiddleware>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseMiddleware<RandomFailureMiddleware>();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+
+    public class RandomFailureMiddleware : IMiddleware
+    {
+        private Random _rand;
+
+        public RandomFailureMiddleware()
+        {
+            _rand = new Random();
+        }
+
+        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            if (_rand.NextDouble() >= 0.5)
+            {
+                throw new Exception("Computer says no.");
+            }
+            return next(context);
+        }
+    }
+}
---
+++ RetryApp/RetryApp/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RetryApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Values { get; set; }
+        public async Task OnGet([FromServices]IHttpClientFactory factory)
+        {
+            var client = factory.CreateClient("api");
+            Values = await client.GetStringAsync("https://localhost:44391/api/values");
+        }
+    }
+}
